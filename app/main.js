2efe00d1f9f642901d2786789685544b1429e6f0
Krishna Paulraj <krish@Krishnas-MacBook-Air.local>
2024-09-02 22:13:24 +0530

    pass stage

--- app/main.js
+++ app/main.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const zlib = require("zlib");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
@@ -11,9 +12,14 @@ switch (command) {
   case "init":
     createGitDirectory();
     break;
+  case "cat-file":
+    createObjectDirectory();
+    break;
   default:
     throw new Error(`Unknown command ${command}`);
 }
+
+// functions
 function createGitDirectory() {
   fs.mkdirSync(path.join(process.cwd(), ".git"), { recursive: true });
   fs.mkdirSync(path.join(process.cwd(), ".git", "objects"), {
@@ -25,4 +31,23 @@ function createGitDirectory() {
     "ref: refs/heads/main\n",
   );
   console.log("Initialized git directory");
+}
+
+function createObjectDirectory() {
+  const hash = process.argv[4];
+  const content = fs.readFileSync(
+    path.join(
+      process.cwd(),
+      ".git",
+      "objects",
+      hash.slice(0, 2),
+      hash.slice(2, hash.length + 1),
+    ),
+  );
+
+  zlib.inflate(content, (err, res) => {
+    if (!err) {
+      console.log(res.toString().split("/0")[1]);
+    }
+  });
 }
