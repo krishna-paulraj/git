c09d17bab1baf36a3abffd6d53a5432c69041bbb
Krishna Paulraj <krish@Krishnas-MacBook-Air.local>
2024-09-03 22:52:12 +0530

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,13 +1,13 @@
 const fs = require("fs");
 const path = require("path");
 const zlib = require("zlib");
+const crypto = require("crypto");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 // console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 const command = process.argv[2];
-const hash = process.argv[4];
 
 switch (command) {
   case "init":
@@ -15,6 +15,9 @@ switch (command) {
     break;
   case "cat-file":
     createObjectDirectory();
+    break;
+  case "hash-object":
+    createHashObject();
     break;
   default:
     throw new Error(`Unknown command ${command}`);
@@ -35,6 +38,7 @@ function createGitDirectory() {
 }
 
 function createObjectDirectory() {
+  const hash = process.argv[4];
   const content = fs.readFileSync(
     path.join(
       process.cwd(),
@@ -47,4 +51,27 @@ function createObjectDirectory() {
 
   const res = zlib.inflateSync(content);
   process.stdout.write(res.toString().split("\x00")[1]);
+}
+
+function createHashObject() {
+  const file = process.argv[4];
+  const size = fs.readFileSync(file).length;
+  const sha1 = crypto.createHash("sha1");
+  sha1.update(`blob ${size}\0${fs.readFileSync(file).toString()}`);
+  const hash = sha1.digest("hex");
+
+  const objectDir = path.join(".git", "objects", hash.slice(0, 2));
+  const objectFile = path.join(objectDir, hash.slice(2));
+
+  fs.mkdirSync(path.join(objectDir), {
+    recursive: true,
+  });
+
+  const content = zlib.deflateSync(
+    `blob ${size}\0${fs.readFileSync(file).toString()}`,
+  );
+
+  fs.writeFileSync(objectFile, content);
+
+  process.stdout.write(hash);
 }
